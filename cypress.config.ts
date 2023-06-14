82eb86764417eeb6c5550b36f9389713e031a497
Richard Hounslow <rhounslow@gmail.com>
2023-06-14 12:14:09 +1000

    integrate applitools

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,16 +1,19 @@
 import { defineConfig } from "cypress";
+import eyesPlugin from "@applitools/eyes-cypress";
 
-export default defineConfig({
-  component: {
-    devServer: {
-      framework: "create-react-app",
-      bundler: "webpack",
+export default eyesPlugin(
+  defineConfig({
+    component: {
+      devServer: {
+        framework: "create-react-app",
+        bundler: "webpack",
+      },
     },
-  },
 
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    e2e: {
+      setupNodeEvents(on, config) {
+        // implement node event listeners here
+      },
     },
-  },
-});
+  })
+);
--- cypress/e2e/0-nearmap-test/app_spec_all_applitools.cy.ts
+++ cypress/e2e/0-nearmap-test/app_spec_all_applitools.cy.ts
@@ -1,4 +1,5 @@
 /// <reference types="cypress" />
+import "@applitools/eyes-cypress/commands";
 
 describe("Functional test", () => {
   beforeEach(() => {
@@ -6,16 +7,27 @@ describe("Functional test", () => {
   });
 
   it("renders the title", () => {
+    cy.eyesOpen({ appName: "nearmap-test", testName: "Title" });
     cy.get("h1").contains("Nearmap Test");
     cy.screenshot();
+    cy.eyesCheckWindow();
+    cy.eyesClose();
   });
 
   it("renders the canvas", () => {
+    cy.eyesOpen({ appName: "nearmap-test", testName: "Canvas exists" });
     cy.get("canvas").should("exist");
     cy.screenshot();
+    cy.eyesCheckWindow();
+    cy.eyesClose();
   });
 
   it("iterates through JSON, clicks position value, shows each objects detail in the Info Box", () => {
+    cy.eyesOpen({
+      appName: "nearmap-test",
+      testName: "See correct InfoBox and selected Marker",
+    });
+
     cy.fixture("map-data.json").then((jsonData) => {
       jsonData.forEach((entry) => {
         let [positionX, positionY] = entry.position;
@@ -27,12 +39,20 @@ describe("Functional test", () => {
           cy.wrap($canvas).scrollIntoView().click(positionX, positionY);
           cy.wrap($canvas).click(positionX, positionY);
           cy.screenshot();
+          cy.eyesCheckWindow();
         });
       });
     });
+
+    cy.eyesClose(); // close after all iterations
   });
 
   it("should click off any marker position, the Info Box should disappear, no markers selected ", () => {
+    cy.eyesOpen({
+      appName: "nearmap-test",
+      testName: "Not see Info Box or selected Marker",
+    });
+
     cy.get("canvas").then(($canvas: JQuery<HTMLCanvasElement>) => {
       // Get dimension of the canvas
       const canvasWidth = $canvas.width();
@@ -46,10 +66,13 @@ describe("Functional test", () => {
         cy.wrap($canvas).scrollIntoView().click(canvasCenterX, canvasCenterY);
         cy.wrap($canvas).click(canvasCenterX, canvasCenterY);
         cy.screenshot();
+        cy.eyesCheckWindow();
       } else {
         throw new Error("Canvas width or height is undefined");
       }
     });
+
+    cy.eyesClose();
   });
 });
 export {};
